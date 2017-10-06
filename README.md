31465920b0efe1ebce531ab0249ac700f670e9c2
maxleiko <max.tricoire@gmail.com>
2017-10-06 12:22:09 +0200

    I think now are closer to something good

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## diversify-jhipster
-This script will change the given JHipster app **pom.xml** and **bower.json** file using the last 10 versions of respectively **spring-boot-starter-parent** and all **angular**-related dependencies.
+## diversify-mvn
+This script will try to create "valid" clones (as in: the tests passes) of the given Maven project, by randomly changing the dependencies versions.
 
 The generated *diversified* projects will be copied in `gen/`
 
@@ -11,34 +11,12 @@ First, install the needed dependencies of the script:
 npm i
 ```
 
-Then, add a JHipster app next to `gen.js`:
+Then, add a Maven project next to `gen.js`:
 ```sh
 git clone git@github.com:jhipster/jhipster-sample-app.git
 ```
 
-Finally, start the script with the path to the JHipster app as param:
+Finally, start the script with the path to the Maven project as param:
 ```sh
 node gen.js jhipster-sample-app
 ```
-
-Expected output:
-```
-╭leiko@kevtop2 ~/dev/diversify-jhipster
-╰➤ node gen.js jhipster-sample-app
-Created: gen/jhipster-sample-app-0 with Spring 1.4.5.RELEASE and Angular 1.6.0-rc.2
-Created: gen/jhipster-sample-app-1 with Spring 1.4.6.RELEASE and Angular 1.6.0
-Created: gen/jhipster-sample-app-2 with Spring 1.4.7.RELEASE and Angular 1.5.10
-Created: gen/jhipster-sample-app-3 with Spring 1.5.1.RELEASE and Angular 1.6.1
-Created: gen/jhipster-sample-app-4 with Spring 1.5.2.RELEASE and Angular 1.5.11
-Created: gen/jhipster-sample-app-5 with Spring 1.5.3.RELEASE and Angular 1.6.2
-Created: gen/jhipster-sample-app-6 with Spring 1.5.4.RELEASE and Angular 1.6.3
-Created: gen/jhipster-sample-app-7 with Spring 1.5.5.RELEASE and Angular 1.6.4
-Created: gen/jhipster-sample-app-8 with Spring 1.5.6.RELEASE and Angular 1.6.5
-Created: gen/jhipster-sample-app-9 with Spring 1.5.7.RELEASE and Angular 1.6.6
-Done.
-```
-
-### TODOs:  
- - [ ] Smartify the SemVer handling (for now it's just the 10 last)
- - [ ] Add more diversification (not only Spring boot parent & Angular, but all deps)
- - [ ] Buy a bike
--- gen.js
+++ gen.js
@@ -2,65 +2,85 @@ process.on('unhandledRejection', (err) => {
   throw err;
 });
 
+const fs = require('fs-extra');
 const path = require('path');
 const chalk = require('chalk');
+const commander = require('commander');
+const pkgJson = require('./package.json');
 const readFiles = require('./lib/read-files');
 const mvnDepsVersions = require('./lib/mvn-deps-versions');
-const bowerDepsVersions = require('./lib/bower-deps-versions');
-const copy = require('./lib/copy');
-const modifyPom = require('./lib/modify-pom');
-const modifyBower = require('./lib/modify-bower');
-const randomDeps = require('./lib/random-deps');
+const createCopy = require('./lib/create-copy');
 
-const JHIPSTER_PATH = process.argv[2];
-const NB_COPIES = process.argv[3] || 10;
+let PROJECT_PATH;
 
-console.log('Using JHipster app at ' + chalk.gray(JHIPSTER_PATH));
-console.log('Reading ' + chalk.gray('pom.xml') + ' and ' + chalk.gray('bower.json'));
-readFiles(JHIPSTER_PATH)
-  .then(({ pom, bower }) => {
-    console.log(chalk.green('Done.') + '\n');
-    console.log('Retrieving the 10 latest versions of ' + chalk.gray('Maven') + ' and ' + chalk.gray('Bower') + ' dependencies');
-    return mvnDepsVersions(pom)
+const program = new commander.Command(pkgJson.name)
+  .version(pkgJson.version)
+  .arguments('<project-directory>')
+  .usage(`${chalk.green('<project-directory>')} [options]`)
+  .action((name) => PROJECT_PATH = name)
+  .option('-n, --nb-clones <n>', 'Number of clones generated', parseInt, 10)
+  .option('-d, --dep-versions <n>', 'How many different version of each dep should be crawled from Maven Central', parseInt, 3)
+  .option('-o, --output <path>', 'Output dir of the generated clones', 'gen')
+  .option('--dry-run', 'Generate clones without executing the tests')
+  .option('--show-logs', 'Pipe tests std out/err to this process')
+  .parse(process.argv);
+
+if (typeof PROJECT_PATH === 'undefined') {
+  console.error('Please specify a project directory:');
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')}`
+  );
+  console.log();
+  console.log('For example:');
+  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('my-mvn-project')}`);
+  console.log();
+  console.log(
+    `Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`
+  );
+  process.exit(1);
+}
+
+if (program.dryRun) {
+  process.env.DRY_RUN = true;
+}
+
+if (program.showLogs) {
+  process.env.SHOW_LOGS = true;
+}
+
+const nbClones = [];
+for (let i=0; i < program.nbClones; i++) {
+  nbClones.push(0);
+}
+
+console.log();
+console.log('Using Maven project at ' + chalk.gray(PROJECT_PATH));
+console.log(`Reading ${chalk.gray('pom.xml')}`);
+readFiles(PROJECT_PATH)
+  .then(({ pom }) => {
+    console.log(chalk.green('Done.'));
+    console.log();
+
+    console.log(`Retrieving the ${program.depVersions} latest versions of the dependencies (${chalk.gray('Maven')})`);
+    return mvnDepsVersions(pom, program.depVersions)
       .then((mvnDeps) => {
-        return bowerDepsVersions(bower)
-          .then((bowerDeps) => ({ mvnDeps, bowerDeps }));
+        return fs.emptyDir(program.output)
+          .then(() => mvnDeps);
       })
-      .then(({ mvnDeps, bowerDeps }) => {
-        console.log(chalk.blue('Maven deps count:') + ' ' + mvnDeps.length);
-        console.log(chalk.blue('Bower deps count:') + ' ' + bowerDeps.length);
-        console.log(chalk.green('Done.') + '\n');
+      .then((mvnDeps) => {
+        console.log(chalk.blue('Dependencies count:') + ' ' + mvnDeps.length);
+        console.log();
 
-        console.log(`Creating ${chalk.blue(NB_COPIES)} copies of ${chalk.gray(JHIPSTER_PATH)}`);
-        const tasks = [];
-        for (let i=0; i < NB_COPIES; i++) {
-          const dest = path.join('gen', JHIPSTER_PATH + '-' + i);
-
-          const randomMvnDeps = randomDeps(mvnDeps);
-          const randomBowerDeps = randomDeps(bowerDeps);
-
-          const newPom = modifyPom(pom, randomMvnDeps);
-          const newBower = modifyBower(bower, randomBowerDeps);
-
-          tasks.push(copy(newPom, newBower, JHIPSTER_PATH, dest)
-            .then(() => {
-              console.log(` - ${chalk.cyan(dest)}`);
-              console.log(`     ${chalk.gray(randomMvnDeps.length + ' maven deps updated:')}`);
-              randomMvnDeps.forEach((mvnDep) => {
-                console.log(`       - ${mvnDep.g}:${mvnDep.a}:${chalk.blue(mvnDep.version)}`);
-              });
-              console.log();
-              console.log(`     ${chalk.gray(randomBowerDeps.length + ' bower deps updated:')}`);
-              randomBowerDeps.forEach((bowerDep) => {
-                console.log(`       - ${bowerDep.name}@${chalk.blue(bowerDep.version)}`);
-              });
-              console.log();
-            }));
-        }
-
-        return Promise.all(tasks);
+        console.log(`Creating ${chalk.blue(program.nbClones)} cop${program.nbClones === 1 ? 'y':'ies'} of ${chalk.gray(PROJECT_PATH)}`);
+        return nbClones.map((ignore, i) => {
+          const newPom = JSON.parse(JSON.stringify(pom));
+          const dest = path.join(program.output, PROJECT_PATH + '-' + i);
+          return () => createCopy(newPom, mvnDeps, i + 1, PROJECT_PATH, dest);
+        });
       });
     })
-    .then(() => {
-      console.log(chalk.green('Bye.'));
+    .then((tasks) => {
+      return tasks.reduce((prev, next) => {
+        return prev.then(next);
+      }, Promise.resolve());
     });
